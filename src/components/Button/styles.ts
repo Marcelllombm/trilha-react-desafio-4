7e0fbed9e1236d3d44aca04e08f41cab8cac429a
Marcelllombm <melombm@hotmail.com>
2022-12-19 20:30:23 -0300

    disabled adicionado no botão

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 
-
 export const ButtonContainer = styled.button`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
-    color: #FFF;
-
+    background-color:${(props)=>props.disabled?"#81259D":' #f0f0f0'};
+    color: ${(props)=>props.disabled?"#ffffff":' #000000'};;
+    pointer-events:${(props)=>props.disabled?null:'none'};
+  
     border: 1px solid #81259D;
     border-radius: 21px;
-
+    button:disabled{
+        background: red;
+    }
     &:hover {
         opacity: 0.6;
         cursor:pointer;
